972b56d0006abd7b5a5b11624d2ad296f05e1158
Vishal-V-D <vishalvd006@gmail.com>
2025-09-16 10:57:52 +0530

    feat(user): implement create_user function

--- user_api.py
+++ user_api.py
@@ -3,3 +3,10 @@ from user_model import User
 
 def create_user(user_id, name, email):
     return User(user_id, name, email)
+
+def get_user_info(user):
+    return {
+        "user_id": user.user_id,
+        "name": user.name,
+        "email": user.email
+    }
